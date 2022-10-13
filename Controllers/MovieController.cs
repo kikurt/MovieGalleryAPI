c895d1dcebd3adbe65ed720d1e55e254cbe208d6
kikurt6ki <kikurt6ki@outlook.com>
2022-10-14 00:15:47 +0800

    Add Movie model Add Movie controller

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieGalleryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Id = 1,
+                    Title = "John Wick",
+                    DateReleased = Convert.ToDateTime("10/09/2014"),
+                    Genre = "Action",
+                    Ratings = 4.6,
+                }
+            };
+            return Ok(movies);
+        }
+        
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieGalleryAPI
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public double Ratings { get; set; }
+        public DateTime DateReleased { get; set; }
+
+    }
+}
